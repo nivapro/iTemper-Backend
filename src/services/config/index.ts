736c5d1d6155a63aada96647f1175e58e52e5793
Torbjörn Vading <torbjorn.vading@nivapro.com>
2020-05-20 21:35:01 +0200

    Fix db connection options and config debug

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -1,14 +1,14 @@
 export const JWT_SECRET = process.env["JWT_SECRET"];
 
 if (!JWT_SECRET) {
-    console.log("No JWT secret string. Set JWT_SECRET environment variable.");
+    console.log("Config, No JWT secret string. Set JWT_SECRET environment variable.");
     process.exit(1);
 }
 export const SALT = process.env["SALT"];
 
 
 if (!SALT) {
-    console.log("No SALT secret string. Set SALT environment variable.");
+    console.log("Config, No SALT secret string. Set SALT environment variable.");
     process.exit(1);
 }
 
@@ -16,7 +16,7 @@ export const LOG_LEVEL = process.env["LOG_LEVEL"];
 
 
 if (!process.env["LOG_LEVEL"]) {
-    console.log("Set LOG_LEVEL environment variable.");
+    console.log("Config, Set LOG_LEVEL environment variable.");
     process.exit(1);
 }
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -26,13 +26,20 @@ const MONGODB_PORT = process.env.MONGODB_PORT;
 let userDBConnectionStr: string = MONGODB_URI  + ":" + parseInt(MONGODB_PORT);
 
 export function setUserDBConnectionString(connectionString: string) {
+    console.log("Config, Set setUserDBConnectionString: " + connectionString);
     userDBConnectionStr = connectionString;
 }
 
 export function userDBConnectionString(): Promise<string> {
-    return new Promise (resolve => { resolve(userDBConnectionStr); });
+    return new Promise (resolve => {
+        console.log("Config, Set userDBConnectionString: " + userDBConnectionStr);
+        resolve(userDBConnectionStr);
+    });
 }
 
 export function tenantDBConnectionString(tenantID: string): Promise<string> {
-    return new Promise (resolve => { resolve( userDBConnectionStr += + tenantID); });
+    return new Promise (resolve => {
+        console.log("Config, Set tenantDBConnectionString: " + tenantID);
+        resolve( userDBConnectionStr += "/" + tenantID);
+    });
 }
