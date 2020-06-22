65e4a914fbd8936a8e57d11c606ff7b8ed37fb90
Torbjörn Vading <torbjorn.vading@nivapro.com>
2020-06-22 03:46:42 +0200

    Enhance config logging

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -1,39 +1,36 @@
-export const JWT_SECRET = process.env["JWT_SECRET"];
+import chalk from "chalk";
 
-if (!JWT_SECRET) {
-    console.log("Config, No JWT secret string. Set JWT_SECRET environment variable.");
-    process.exit(1);
+export const JWT_SECRET = getEnv("JWT_SECRET");
+
+export const SALT = getEnv("SALT");
+
+export const LOG_LEVEL = getEnv("LOG_LEVEL");
+
+const MONGODB_URI = getEnv("MONGODB_URI");
+
+const MONGODB_PORT = getEnv("MONGODB_PORT");
+
+function getEnv(env: string): string {
+    const val = process.env[env];
+    if (!val) {
+        const msg = "console.error: [itemper-backend]: config: No " + env + " environment variable.";
+        const error = chalk.red(msg);
+        console.error(error);
+        process.exit(0);
+    }
+    return val;
 }
-export const SALT = process.env["SALT"];
-
-
-if (!SALT) {
-    console.log("Config, No SALT secret string. Set SALT environment variable.");
-    process.exit(1);
-}
-
-export const LOG_LEVEL = process.env["LOG_LEVEL"];
-
-
-if (!process.env["LOG_LEVEL"]) {
-    console.log("Config, Set LOG_LEVEL environment variable.");
-    process.exit(1);
-}
-const MONGODB_URI = process.env.MONGODB_URI;
-
-const MONGODB_PORT = process.env.MONGODB_PORT;
-
 let userDBConnectionStr: string = MONGODB_URI  + ":" + parseInt(MONGODB_PORT);
 
 export function setUserDBConnectionString(connectionString: string) {
-    console.log("Config, Set setUserDBConnectionString: " + connectionString);
+    console.info("console.info: [itemper-backend]: config.setUserDBConnectionString: " + connectionString);
     userDBConnectionStr = connectionString;
 }
 
 export function userDBConnectionString(): Promise<string> {
     return new Promise (resolve => {
         const connectionString = userDBConnectionStr + "/Directory";
-        console.log("Config, Set userDBConnectionString: " + connectionString);
+        console.info("console.info: [itemper-backend]: config.userDBConnectionString: " + connectionString);
         resolve(connectionString);
     });
 }
@@ -41,7 +38,7 @@ export function userDBConnectionString(): Promise<string> {
 export function tenantDBConnectionString(tenantID: string): Promise<string> {
     return new Promise (resolve => {
         const connectionString = userDBConnectionStr + "/" + tenantID;
-        console.log("Config, Set tenantDBConnectionString: " + connectionString);
+        console.info("console.info: config.tenantDBConnectionString: " + connectionString);
         resolve( connectionString);
     });
 }
