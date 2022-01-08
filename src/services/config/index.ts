e57802b18d296c1875fd6f166836db22afa2115b
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-01-08 13:38:05 +0100

    Read and log all env before existing application (#46)
    * Update debian config scripts
    
    * Include mongoose error at user db connection error
    
    * Upgrade Typescript, Node, Mongoose, Eslint
    
    * Fix eslint errors and warning
    
    * Use docker as non root and improve logging of env variables
    
    * Read and log all envs before existing
    
    * Refactor config logging

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -1,5 +1,38 @@
+// Uses an own log since logger depends on this file
 import chalk from "chalk";
+function log(message: string, error = false) {
+    // return `${level}: ${timestamp} [${label}]: ${message}`;
+    const time = new Date().toISOString();
+    const msg = time + " [itemper-backend]: config: " + message;
+    if (error) {
+        const error = chalk.red("error: " + msg);
+        console.error (error);
+    } else {
+        console.log ("info: " +(msg));
+    }
+}
+// Checks that all configuration is complete
+let configError = false;
+function checkConfiguration () {
+    if (configError) {
+        log("Configuration errors found, existing application");
+        process.exit(0)
+    }
+}
+// Verifies a environment variable
+function getEnv(env: string): string {
+    const val = process.env[env];
+    if (val) {
+        log(env + " environment variable found, [" + val + "]");
+    } 
+    else {
+        configError = true;
+        log("No " + env + " environment variable.", configError);
+    }
+    return val;
+}
 
+// Get all environment variables
 export const JWT_SECRET = getEnv("JWT_SECRET");
 
 export const SALT = getEnv("SALT");
@@ -10,6 +43,14 @@ const MONGODB_URI = getEnv("MONGODB_URI");
 
 const MONGODB_PORT = getEnv("MONGODB_PORT");
 
+let userDBConnectionStr: string = MONGODB_URI  + ":" + parseInt(MONGODB_PORT);
+
+checkConfiguration();
+
+export function setUserDBConnectionString(connectionString: string) {
+    log ("setUserDBConnectionString: " + connectionString);
+      userDBConnectionStr = connectionString;
+}
 // Use an own log since logger depends on this file
 function log(message: string, error = false) {
     // return `${level}: ${timestamp} [${label}]: ${message}`;
@@ -23,7 +64,6 @@ function log(message: string, error = false) {
     }
 
 }
-
 function getEnv(env: string): string {
     const val = process.env[env];
     if (val) {
@@ -38,24 +78,21 @@ function getEnv(env: string): string {
 }
 let userDBConnectionStr: string = MONGODB_URI  + ":" + parseInt(MONGODB_PORT);
 
-
 export function setUserDBConnectionString(connectionString: string) {
     log ("config.setUserDBConnectionString: " + connectionString);
     userDBConnectionStr = connectionString;
 }
-
 export function userDBConnectionString(): Promise<string> {
     return new Promise (resolve => {
         const connectionString = userDBConnectionStr + "/Directory";
-        log("config.userDBConnectionString: " + connectionString);
+        log("userDBConnectionString: " + connectionString);
         resolve(connectionString);
     });
 }
-
 export function tenantDBConnectionString(tenantID: string): Promise<string> {
     return new Promise (resolve => {
         const connectionString = userDBConnectionStr + "/" + tenantID;
-        console.info("console.info: config.tenantDBConnectionString: " + connectionString);
+        log("tenantDBConnectionString: " + connectionString);
         resolve( connectionString);
     });
 }
--- src/services/logger/index.ts
+++ src/services/logger/index.ts
@@ -64,7 +64,9 @@ private _name: string;
 
 }
 const log = () => new Log();
-log().info("Logger: New application log created");
+
+log().info("logger: New application log created");
+
 export default log();
 
 export function setLevel(level: string): void {
