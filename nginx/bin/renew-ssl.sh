f7392768c6eb5cea928b7b0b72e0662dfabacdc3
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-03-09 20:32:44 +0100

    Correct misspelling

--- nginx/bin/renew-ssl.sh
+++ nginx/bin/renew-ssl.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/tova/iTemper-Backend/nginx
-$COMPOSE -f docker-compose-production.yml run cerbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE -f docker-compose-production.yml run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
