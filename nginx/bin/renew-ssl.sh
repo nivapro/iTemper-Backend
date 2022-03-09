2680ea2416e420081d834685bfcf27ffa5f6ec78
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-03-09 20:59:17 +0100

    Remove dry-run

--- nginx/bin/renew-ssl.sh
+++ nginx/bin/renew-ssl.sh
@@ -4,6 +4,6 @@ COMPOSE="/usr/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/tova/iTemper-Backend/nginx
-$COMPOSE -f docker-compose-production.yml run certbot renew --dry-run 
+$COMPOSE -f docker-compose-production.yml run certbot renew
 $COMPOSE -f docker-compose-production.yml kill -s SIGHUP webserver
 $DOCKER system prune -af
