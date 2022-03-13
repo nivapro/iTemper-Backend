ea05b1555afffb904494c9b725f0c7286847dfd9
Torbjörn Vading <torbjorn.vading@gmail.com>
2022-03-13 09:35:52 +0000

    Add logs

--- .devcontainer/clone-repos.sh
+++ .devcontainer/clone-repos.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+echo "BASH SOURCE: ${BASH_SOURCE[0]}"
 script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
-workspaces_folder="$(cd "${script_folder}/.." && pwd)"
+echo "Script folder: ${script_folder}" 
+workspaces_folder="$(cd "${script_folder}/../.." && pwd)"
+echo "workplaces folder: ${workspaces_folder}" 
 
 clone-repo()
 {
@@ -13,10 +16,10 @@ clone-repo()
 }
 
 if [ "${CODESPACES}" = "true" ]; then
-    # Remove the default credential helper
+    echo "Remove the default credential helper"
     sudo sed -i -E 's/helper =.*//' /etc/gitconfig
 
-    # Add one that just uses secrets available in the Codespace
+    echo "Add one that just uses secrets available in the Codespace"
     git config --global credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GH_TOKEN}"; }; f'
 fi
 
